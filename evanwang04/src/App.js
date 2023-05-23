8f606af946638d2cc446b4fcc9f080fa03b894c3
Evan <60945611+EvanWang04@users.noreply.github.com>
2023-05-22 23:01:59 -0400

    Updating website

--- evanwang04/src/App.js
+++ evanwang04/src/App.js
@@ -1,9 +1,50 @@
 import './App.css';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import DescriptionIcon from '@mui/icons-material/Description';
+import EmailIcon from '@mui/icons-material/Email';
 
 function App() {
   return (
-    <div>
-      HELLLOOOOOO
+    <div class="app-container">
+      {/* <div class="header title">
+        Welcome to my digital corner!
+      </div> */}
+      <div class="welcome-container">
+        <div class="welcome-title title">
+          Welcome!
+        </div>
+        <div class="welcome-content content">
+          Thanks for stopping by and welcome to my digital corner!
+        </div>
+      </div>
+      <div class="about-container">
+        <div class="about-title title">
+          Who Am I?
+        </div>
+        <div class="about-content content">
+          Hey, I'm Evan 👋
+          <br></br>
+          <br></br>
+          I'm a software engineering student at the University of Waterloo, currently interning at Ford as a backend software engineer.
+          <br></br>
+          <br></br>
+          My journey revolves around creating impactful software, ones that truly make a difference. It's not just about codes and algorithms, but about how these digital products touch lives and change the world.          <br></br>
+          <br></br>
+          Interested to know more, collaborate, or just chat about tech and life? Don't hesitate to connect!
+        </div>
+      </div>
+      <div class="links-container">
+        <div class="links-title title">
+          Find me on
+        </div>
+        <div class="links-content content">
+          <a class="link-item" href="https://www.linkedin.com/in/evanwang04/" target="_blank" rel="noreferrer"><LinkedInIcon fontSize='small'></LinkedInIcon><span class="underline">LinkedIn</span></a>
+          <a class="link-item" href="https://github.com/EvanWang04" target="_blank" rel="noreferrer"><GitHubIcon fontSize='small'></GitHubIcon><span class="underline">GitHub</span></a>
+          <a class="link-item" href="https://www.linkedin.com/in/evanwang04/" target="_blank" rel="noreferrer"><DescriptionIcon fontSize='small'></DescriptionIcon><span class="underline">Resume</span></a>
+          <a class="link-item" href="mailto:exwang@uwaterloo.ca" target="_blank" rel="noreferrer"><EmailIcon fontSize='small'></EmailIcon><span class="underline">Email</span></a>
+        </div>
+      </div>
     </div>
   );
 }
