a03ba79ebe5eb87369b55bff1f3f3ef4500ed586
Evan Wang <exwang@uwaterloo.ca>
2024-06-18 17:57:05 -0400

    Refactor

--- evanwang04/src/App.js
+++ evanwang04/src/App.js
@@ -1,47 +1,26 @@
 import './App.css';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import DescriptionIcon from '@mui/icons-material/Description';
 import EmailIcon from '@mui/icons-material/Email';
 
 function App() {
   return (
     <div class="app-container">
-      {/* <div class="header title">
-        Welcome to my digital corner!
-      </div> */}
-      <div class="welcome-container">
-        <div class="welcome-title title">
-          Welcome!
+      <div class="content">
+        <div class="welcome-title">
+          Hi, I'm Evan Wang
         </div>
-        <div class="welcome-content content">
-          Thanks for stopping by and welcome to my digital corner!
+        <div class="welcome-content">
+          I'm a software engineering student at the University of Waterloo, seeking Fall 2024 internships (September 2024 - December 2024).
+          <br/><br/>
+          Previously, I've interned at <a href="https://www.bankonloop.com/" target="_blank" rel="noreferrer">Loop Financial</a>, <a href="https://www.ford.ca/" target="_blank" rel="noreferrer">Ford Motor Company</a>, and the <a href="https://www.rbcroyalbank.com" target="_blank" rel="noreferrer">Royal Bank of Canada</a>.
+          <br/><br/>
+          Interested to know more, collaborate, or just chat? Don't hesitate to connect.
         </div>
-      </div>
-      <div class="about-container">
-        <div class="about-title title">
-          Who Am I?
-        </div>
-        <div class="about-content content">
-          Hey, I'm Evan 👋
-          <br></br>
-          <br></br>
-          I'm a software engineering student at the University of Waterloo, seeking Fall 2024 internships (Sept - Dec 2024).
-          <br></br>
-          <br></br>
-          I'm focused on building software that genuinely makes a difference. It's not just about coding, but about how these tools can positively impact people's lives.          <br></br>
-          <br></br>
-          Interested to know more, collaborate, or just chat about tech and life? Don't hesitate to connect!
-        </div>
-      </div>
-      <div class="links-container">
-        <div class="links-title title">
-          Find me on
-        </div>
-        <div class="links-content content">
+        <div class="links-container">
           <a class="link-item" href="https://www.linkedin.com/in/evanwang04/" target="_blank" rel="noreferrer"><LinkedInIcon fontSize='small'></LinkedInIcon><span class="underline">LinkedIn</span></a>
           <a class="link-item" href="https://github.com/EvanWang04" target="_blank" rel="noreferrer"><GitHubIcon fontSize='small'></GitHubIcon><span class="underline">GitHub</span></a>
-          <a class="link-item" href="mailto:exwang@uwaterloo.ca" target="_blank" rel="noreferrer"><EmailIcon fontSize='small'></EmailIcon><span class="underline">Email</span></a>
+          <a class="link-item" href="mailto:evan.wang@uwaterloo.ca" target="_blank" rel="noreferrer"><EmailIcon fontSize='small'></EmailIcon><span class="underline">Email</span></a>
         </div>
       </div>
     </div>
